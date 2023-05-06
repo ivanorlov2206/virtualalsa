c9c1563f69ab7db735201856d90f5af0b153c17f
Ivan Orlov <ivan.orlov0322@gmail.com>
2023-05-06 23:05:46 +0400

    Refactor code, remove seq mode, update README
    The sequential filling mode duplicates the pattern repeating mode, so
    we don't need both of them. The sequential filling mode can be
    implemented using the pattern repeating mode.
    
    Update the README and add description of new modes into it.

--- README.md
+++ README.md
@@ -16,23 +16,30 @@ And you have 3 seconds of beautiful white noise...
 
 The driver itself has three modes for pattern generating:
 0. The random sequence of bytes
-1. The looped monotonically increasing sequence of bytes (0, 1, 2, ..., FF, 0, 1, 2, ...)
-2. The pattern repeating mode
+1. The pattern repeating mode
 
 To change the module mode, write the corresponding option to the module parameter:
 ```
 echo 1 > /sys/module/alsav/parameters/fill_mode
 ```
-The most interesting is the third mode, where you can specify the pattern to repeat:
+The most interesting is the second mode, where you can specify the pattern to repeat:
 ```
-echo some_pattern > /sys/module/alsav/parameters/fill_pattern
+echo some_pattern > /sys/kernel/debug/alsav/fill_pattern
 ```
+The pattern can be up to 1024 bytes length.
 
 Also, it can be used for checking the playback functionality.
-If the playback buffer contains the looped monotonically increasing sequence of
-bytes (0, 1, 2, ..., FF, 0, 1, ...) the test will success, and the
+If the playback buffer contains the looped pattern (which you set in fill_pattern) the test will
+success, and the
 ```
 /sys/kernel/debug/alsav/pc_test
 ```
 file will contain '1' after the pcm closing. Otherwise, if the buffer is
 corrupted somehow, this debugfs file will contain '0'.
+
+If you want to test the playback functionality as mentioned above, the pattern must not contain
+zeros - otherwise the test results will be incorrect.
+## Reset IOCTL redefinition
+This driver can be used to test the 'RESET' ioctl redefinition through ALSA API. To test it, reset
+the pcm (for example, with snd_pcm_reset call), and check this debugfs file (in case if the new
+IOCTL triggers, it will contain '1', otherwise - '0').
--- alsav.c
+++ alsav.c
@@ -4,34 +4,29 @@
  *
  * Copyright 2023 Ivan Orlov <ivan.orlov0322@gmail.com>
  *
- * This is a simple virtual ALSA driver, aimed at the ALSA middle layer testing.
+ * This is a simple virtual ALSA driver, which can be used for audio applications/ALSA middle layer
+ * testing or fuzzing.
  * It can:
  *	- Simulate 'playback' and 'capture' actions
  *	- Generate random or pattern-based capture data
- *	- Check playback buffer for containing looped and monotonically increasing sequence of
- *	bytes (0, 1, 2, ..., FF, 0, 1, 2, ...), and notify about the results through the debugfs
- *      entry
+ *	- Check playback buffer for containing looped template, and notify about the results
+ *	through the debugfs entry
  *	- Register custom RESET ioctl and notify when it is called through the debugfs entry
  *
- * You can find the corresponding selftest in the 'alsa' selftests folder.
+ * The driver supports framerates from 8 kHz to 48 kHz, however on the higher framerates it works
+ * with delays, so it is better to choose lower framerates (for example, 8 kHz).
  *
- * Also it can be used as a template for other virtual drivers.
+ * You can find the corresponding selftest in the 'alsa' selftests folder.
  */
 
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/slab.h>
-#include <linux/fs.h>
 #include <sound/pcm.h>
 #include <sound/core.h>
-#include <sound/control.h>
-#include <sound/initval.h>
 #include <linux/dma-mapping.h>
 #include <linux/platform_device.h>
 #include <linux/timer.h>
-#include <linux/delay.h>
 #include <linux/random.h>
-#include <linux/string.h>
 #include <linux/debugfs.h>
 
 #define DEVNAME "alsavd"
@@ -40,16 +35,17 @@
 #define TIMER_INTERVAL (HZ / TIMER_PER_SEC)
 
 #define FILL_MODE_RAND	0
-#define FILL_MODE_SEQ	1
-#define FILL_MODE_PAT	2
+#define FILL_MODE_PAT	1
+
+#define MAX_PATTERN_LEN 1024
 
 static int index = -1;
 static char *id = "alsav";
 static bool enable = 1;
 
-static short fill_mode = FILL_MODE_SEQ;
-static char *fill_pattern = "abacaba";
-
+static short fill_mode = FILL_MODE_PAT;
+static char fill_pattern[MAX_PATTERN_LEN] = "abacaba";
+static ssize_t pattern_len = 7;
 static struct timer_list timer;
 
 static u8 playback_capture_test;
@@ -63,18 +59,17 @@ MODULE_PARM_DESC(id, "ID string for " CARD_NAME " soundcard");
 module_param(enable, bool, 0444);
 MODULE_PARM_DESC(enable, "Enable " CARD_NAME " soundcard.");
 module_param(fill_mode, short, 0600);
-MODULE_PARM_DESC(fill_mode, "Buffer fill mode: rand(0) or seq(1)");
-module_param(fill_pattern, charp, 0600);
+MODULE_PARM_DESC(fill_mode, "Buffer fill mode: rand(0) or pattern(1)");
 
 struct alsav {
 	struct snd_pcm_substream *substream;
 	struct snd_pcm *pcm;
 	size_t buf_pos;
 	size_t period_pos;
-	size_t b_read;
-	bool wrong_zero;
+	size_t b_rw;
 	bool is_buf_corrupted;
 	size_t period_bytes;
+	size_t total_bytes;
 	struct platform_device *pdev;
 	struct snd_card *card;
 };
@@ -86,9 +81,9 @@ static struct snd_pcm_hardware snd_alsav_hw = {
 		 SNDRV_PCM_INFO_BLOCK_TRANSFER |
 		 SNDRV_PCM_INFO_MMAP_VALID),
 	.formats =		SNDRV_PCM_FMTBIT_S16_LE,
-	.rates =		SNDRV_PCM_RATE_8000,
+	.rates =		SNDRV_PCM_RATE_8000_48000,
 	.rate_min =		8000,
-	.rate_max =		8000,
+	.rate_max =		48000,
 	.channels_min =		1,
 	.channels_max =		1,
 	.buffer_bytes_max =	32768,
@@ -98,39 +93,75 @@ static struct snd_pcm_hardware snd_alsav_hw = {
 	.periods_max =		1024,
 };
 
-// Check one block of the buffer
+static inline void inc_buf_pos(size_t by, size_t bytes)
+{
+	alsav->total_bytes += by;
+	alsav->buf_pos += by;
+	alsav->buf_pos %= bytes;
+}
+
+/*
+ * Check one block of the buffer. Here we iterate the buffer until we find '0'. This condition is
+ * necessary because we need to detect when the reading/writing ends, so we assume that the pattern
+ * doesn't contain zeros.
+ */
 static void check_buf_block(struct alsav *alsav, struct snd_pcm_runtime *runtime)
 {
 	size_t i;
 	u8 current_byte;
 
-	for (i = 0; i < alsav->b_read; i++) {
-		/*
-		 * Set wrong_zero if we found '0' on position where it shouldn't be.
-		 * It can occur if buffer is corrupted or if we reach its end -
-		 * In second case we can't find any other nums further except '0'
-		 * If we found any other non-zero byte after wrong placed '0', the buffer
-		 * is corrupted. It is definitely corrupted if we found wrong placed
-		 * non-zero byte
-		 */
+	for (i = 0; i < alsav->b_rw; i++) {
 		current_byte = runtime->dma_area[alsav->buf_pos];
-		if (current_byte && (alsav->wrong_zero || current_byte != alsav->buf_pos % 256)) {
+		if (!current_byte)
+			break;
+		if (current_byte != fill_pattern[alsav->total_bytes % pattern_len]) {
 			alsav->is_buf_corrupted = true;
 			break;
-		} else if (!current_byte && alsav->buf_pos % 256 != 0) {
-			alsav->wrong_zero = true;
 		}
-		alsav->buf_pos++;
-		alsav->buf_pos %= runtime->dma_bytes;
+		inc_buf_pos(1, runtime->dma_bytes);
 	}
-	alsav->buf_pos += alsav->b_read - i;
-	alsav->buf_pos %= runtime->dma_bytes;
+	inc_buf_pos(alsav->b_rw - i, runtime->dma_bytes);
+}
+
+static void fill_block_pattern(struct alsav *alsav, struct snd_pcm_runtime *runtime)
+{
+	size_t i;
+
+	for (i = 0; i < alsav->b_rw; i++) {
+		runtime->dma_area[alsav->buf_pos] = fill_pattern[alsav->total_bytes % pattern_len];
+		inc_buf_pos(1, runtime->dma_bytes);
+	}
+}
+
+static void fill_block_random(struct alsav *alsav, struct snd_pcm_runtime *runtime)
+{
+	size_t in_cur_block = runtime->dma_bytes - alsav->buf_pos;
+
+	if (alsav->b_rw <= in_cur_block) {
+		get_random_bytes(&runtime->dma_area[alsav->buf_pos], alsav->b_rw);
+	} else {
+		get_random_bytes(&runtime->dma_area[alsav->buf_pos], in_cur_block);
+		get_random_bytes(runtime->dma_area, alsav->b_rw - in_cur_block);
+	}
+	inc_buf_pos(alsav->b_rw, runtime->dma_bytes);
+}
+
+static void fill_block(struct alsav *alsav, struct snd_pcm_runtime *runtime)
+{
+	switch (fill_mode) {
+	case FILL_MODE_RAND:
+		fill_block_random(alsav, runtime);
+		break;
+	case FILL_MODE_PAT:
+		fill_block_pattern(alsav, runtime);
+		break;
+	}
 }
 
 /*
  * Here we iterate through the buffer by (buffer_size / iterates_per_second) bytes.
- * We need it to simulate the hardware pointer moving, and notify the PCM middle layer about
- * period elapsed.
+ * The driver uses timer to simulate the hardware pointer moving, and notify the PCM middle layer
+ * about period elapsed.
  */
 static void timer_timeout(struct timer_list *data)
 {
@@ -142,10 +173,12 @@ static void timer_timeout(struct timer_list *data)
 
 	if (alsav->substream->stream == SNDRV_PCM_STREAM_PLAYBACK && !alsav->is_buf_corrupted)
 		check_buf_block(alsav, runtime);
+	else if (alsav->substream->stream == SNDRV_PCM_STREAM_CAPTURE)
+		fill_block(alsav, runtime);
 	else
-		alsav->buf_pos = (alsav->buf_pos + alsav->b_read) % runtime->dma_bytes;
+		inc_buf_pos(alsav->b_rw, runtime->dma_bytes);
 
-	alsav->period_pos += alsav->b_read;
+	alsav->period_pos += alsav->b_rw;
 	if (alsav->period_pos >= alsav->period_bytes) {
 		alsav->period_pos %= alsav->period_bytes;
 		snd_pcm_period_elapsed(alsav->substream);
@@ -162,9 +195,9 @@ static int snd_alsav_pcm_open(struct snd_pcm_substream *substream)
 	runtime->hw = snd_alsav_hw;
 	alsav->substream = substream;
 	alsav->buf_pos = 0;
-	alsav->wrong_zero = false;
 	alsav->is_buf_corrupted = false;
 	alsav->period_pos = 0;
+	alsav->total_bytes = 0;
 
 	playback_capture_test = 0;
 	ioctl_reset_test = 0;
@@ -181,72 +214,17 @@ static int snd_alsav_pcm_close(struct snd_pcm_substream *substream)
 
 	timer_shutdown_sync(&timer);
 	alsav->substream = NULL;
-
 	playback_capture_test = !alsav->is_buf_corrupted;
 	return 0;
 }
 
-static void fill_buffer_seq(void *buf, size_t bytes)
-{
-	size_t i;
-	u8 *buffer = buf;
-
-	for (i = 0; i < bytes; i++)
-		buffer[i] = i % 256;
-}
-
-static void fill_buffer_rand(void *buf, size_t bytes)
-{
-	get_random_bytes(buf, bytes);
-}
-
-static void fill_buffer_pattern(void *buf, size_t bytes)
-{
-	size_t i;
-	u8 *buffer = buf;
-	size_t pattern_len = strlen(fill_pattern);
-
-	for (i = 0; i < bytes; i++)
-		buffer[i] = fill_pattern[i % pattern_len];
-}
-
-static void fill_buffer(struct snd_pcm_runtime *runtime)
-{
-	switch (fill_mode) {
-	case FILL_MODE_RAND:
-		fill_buffer_rand(runtime->dma_area, runtime->dma_bytes);
-		break;
-	case FILL_MODE_PAT:
-		fill_buffer_pattern(runtime->dma_area, runtime->dma_bytes);
-		break;
-	case FILL_MODE_SEQ:
-	default:
-		fill_buffer_seq(runtime->dma_area, runtime->dma_bytes);
-		break;
-	}
-}
-
-/*
- * Here we have the dma buffer already initializated, and can either fill it or check it,
- * depending on the substream mode
- */
 static int snd_alsav_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 
 	alsav->period_bytes = frames_to_bytes(runtime, runtime->period_size);
-	// We want to record RATE samples per sec, it is rate * sample_bytes
-	alsav->b_read = runtime->rate * runtime->sample_bits / 8 / TIMER_PER_SEC;
-	switch (cmd) {
-	case SNDRV_PCM_TRIGGER_START:
-		if (substream->stream == SNDRV_PCM_STREAM_CAPTURE)
-			fill_buffer(runtime);
-		break;
-	case SNDRV_PCM_TRIGGER_STOP:
-		break;
-	default:
-		return -EINVAL;
-	}
+	// We want to record RATE samples per sec, it is rate * sample_bytes bytes
+	alsav->b_rw = runtime->rate * runtime->sample_bits / 8 / TIMER_PER_SEC;
 	return 0;
 }
 
@@ -271,7 +249,6 @@ static int snd_alsav_dev_free(struct snd_device *device)
 
 static void alsav_pdev_release(struct device *dev)
 {
-
 }
 
 static int snd_alsav_pcm_prepare(struct snd_pcm_substream *substream)
@@ -337,7 +314,7 @@ static int snd_alsav_new_pcm(struct alsav *alsav)
 	snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE, &snd_alsav_capture_ops);
 
 	err = snd_pcm_set_managed_buffer_all(pcm, SNDRV_DMA_TYPE_DEV, &alsav->pdev->dev,
-					     16 * 1024, 32 * 1024);
+					     64 * 1024, 64 * 1024);
 	return err;
 }
 
@@ -420,6 +397,47 @@ static struct platform_driver alsav_pdrv = {
 	},
 };
 
+static ssize_t pattern_write(struct file *file, const char __user *buff, size_t len, loff_t *off)
+{
+	ssize_t to_write = len;
+
+	if (*off + to_write > MAX_PATTERN_LEN)
+		to_write = MAX_PATTERN_LEN - *off;
+
+	// Crop silently everything over the buffer
+	if (to_write <= 0)
+		return len;
+
+	if (copy_from_user(fill_pattern + *off, buff, to_write))
+		return -EFAULT;
+	pattern_len = *off + to_write;
+	*off += to_write;
+
+	return to_write;
+}
+
+static ssize_t pattern_read(struct file *file, char __user *buff, size_t len, loff_t *off)
+{
+	ssize_t to_read = len;
+
+	if (*off + to_read >= MAX_PATTERN_LEN)
+		to_read = MAX_PATTERN_LEN - *off;
+	if (to_read <= 0)
+		return 0;
+
+	if (copy_to_user(buff + *off, fill_pattern, to_read))
+		to_read = 0;
+	else
+		*off += to_read;
+
+	return to_read;
+}
+
+static const struct file_operations fill_pattern_fops = {
+	.read = pattern_read,
+	.write = pattern_write,
+};
+
 static int init_debug_files(void)
 {
 	driver_debug_dir = debugfs_create_dir("alsav", NULL);
@@ -427,6 +445,7 @@ static int init_debug_files(void)
 		return PTR_ERR(driver_debug_dir);
 	debugfs_create_u8("pc_test", 0444, driver_debug_dir, &playback_capture_test);
 	debugfs_create_u8("ioctl_test", 0444, driver_debug_dir, &ioctl_reset_test);
+	debugfs_create_file("fill_pattern", 0600, driver_debug_dir, NULL, &fill_pattern_fops);
 
 	return 0;
 }
